2dd9a82cffef738d059a06afe3ef615e6935e3ad
hs222 <40136388+hs222@users.noreply.github.com>
2023-05-08 16:16:36 +0200

    modifications for reading data set files

--- src/main/java/apriori/logic/datageneration/GenerateDataset.java
+++ src/main/java/apriori/logic/datageneration/GenerateDataset.java
@@ -1,10 +1,17 @@
 package apriori.logic.datageneration;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
+import apriori.logic.utility.Property;
 import apriori.logic.utility.TypeDataset;
 
 /**
@@ -30,13 +37,14 @@ public class GenerateDataset {
 			ads = GenerateDataset.generateAlphabeticAbc(maxElements, noTransactions, maxSize, minSize);
 		} else if (types.equals("RandomLetters")) {
 			ads = GenerateDataset.generateAlphabeticRandomLetters(maxElements, noTransactions, maxSize, minSize);
-		} 
+		}
 
 		EnumMap<TypeDataset, String> map = TypeDataset.getMap();
-		for (Map.Entry<TypeDataset,String> entry : map.entrySet()) {			
-			  if(   entry.getKey().toString().equals(types)) {  
-				  ads = GenerateDataset.generateFromFile(entry.getValue().toString(), maxElements, noTransactions, maxSize, minSize);
-			  }
+		for (Map.Entry<TypeDataset, String> entry : map.entrySet()) {
+			if (entry.getKey().toString().equals(types)) {
+				ads = GenerateDataset.generateFromFileResource(entry.getValue().toString(), maxElements, noTransactions,
+						maxSize, minSize);
+			}
 		}
 		return ads;
 	}
@@ -78,8 +86,9 @@ public class GenerateDataset {
 	}
 
 	/**
-	 * for generating data set from csv file (in first column is the weight of an
-	 * item, which is in the second column)
+	 * for generating data set from csv file, file not loaded as a resource, will
+	 * not work in JAR files (in first column is the weight of an item, which is in
+	 * the second column)
 	 * 
 	 * @param fileName             name of the file
 	 * @param numberOfElements     numbers of available items
@@ -94,20 +103,83 @@ public class GenerateDataset {
 		WeightedItemSet itemSet = WeightedItemSet.generateAlphabeticABC(numberOfElements);
 		Sampl sample = new Sampl(itemSet);
 		ads = sample.generateAssociationDataSet(numberOfTransactions, maxSize, minSize);
-		
+
 		File file = new File(fileName);
-		if (file.exists()==true) {			
+		if (file.exists() == true) {
 			try {
 				WeightedItemSet wis = WeightedItemSet.readData(fileName);
 				Sampl sam2 = new Sampl(wis);
 				ads = sam2.generateAssociationDataSet(numberOfTransactions, maxSize, minSize);
 			} catch (IOException e) {
 				e.printStackTrace();
-			} 			
+			}
 		} else {
 			System.out.println("File not found; falling back to default. ");
 
 		}
+		return ads;
+	}
+
+	/**
+	 * for generating data set from csv file, if the saved file is in the resource
+	 * folder (in first column is the weight of an item, which is in the second
+	 * column)
+	 * 
+	 * @param fileName             name of the file
+	 * @param numberOfElements     numbers of available items
+	 * @param numberOfTransactions number of transactions
+	 * @param maxSize              maximum number of items in transactions
+	 * @param minSize              minimum number of items in transactions
+	 * @return ads association data set
+	 */
+	public static AssociationDataSet generateFromFileResource(String fileName, int numberOfElements,
+			int numberOfTransactions, int maxSize, int minSize) {
+
+		AssociationDataSet ads;
+		WeightedItemSet itemSet = WeightedItemSet.generateAlphabeticABC(numberOfElements);
+		Sampl sample = new Sampl(itemSet);
+		ads = sample.generateAssociationDataSet(numberOfTransactions, maxSize, minSize);
+
+		InputStream id = null;
+
+		try (var rs = Property.class.getResourceAsStream(fileName)) {
+			id = rs;
+			List<String[]> content = new ArrayList<>();
+
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(rs))) {
+				String line = "";
+				while ((line = br.readLine()) != null) {
+					content.add(line.split(";"));
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			WeightedItem[] weightedItems = new WeightedItem[content.size()];
+			for (int i = 0; i < content.size(); i++) {
+
+				double dd = 0;
+				try {
+					dd = Double.parseDouble(content.get(i)[0]);
+				} catch (NumberFormatException ex) {
+					double min = 0.1;
+					double max = Math.random() * (10 - min + 1) + min;
+					Random random = new Random();
+					dd = min + random.nextDouble() * (max - min);
+				}
+				WeightedItem wi = new WeightedItem(content.get(i)[1], (dd / 100));
+				weightedItems[i] = wi;
+			}
+
+			WeightedItemSet wis = new WeightedItemSet(weightedItems);
+
+			Sampl sam2 = new Sampl(wis);
+			ads = sam2.generateAssociationDataSet(numberOfTransactions, maxSize, minSize);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		return ads;
 	}
 }
