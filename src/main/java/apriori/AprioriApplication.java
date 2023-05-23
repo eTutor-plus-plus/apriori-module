86e2dd1536f90cdfb9cc3b8ceab864e3c14ebf3d
kevschuetz <kevin.hotz@gmx.at>
2023-05-23 09:46:59 +0200

    docker fixed

--- src/main/java/apriori/AprioriApplication.java
+++ src/main/java/apriori/AprioriApplication.java
@@ -17,16 +17,16 @@ public class AprioriApplication {
 	 * @param args	application starting parameters
 	 */
 	public static void main(String[] args) {
-		System.out.println(Property.getProperty("----------------------CONFIGURATION--------------------"));
-		System.out.println(Property.getProperty("etutorplusplus.link"));
-		System.out.println(Property.getProperty("etutorplusplus.extension.key"));
-		System.out.println(Property.getProperty("semantic.link"));
-		System.out.println(Property.getProperty("semantic.query.endpoint"));
-		System.out.println(Property.getProperty("semantic.update.endpoint"));
-		System.out.println(Property.getProperty("spring.datasource.url"));
-		System.out.println(Property.getProperty("spring.datasource.username"));
-		System.out.println(Property.getProperty("spring.datasource.password"));
-		System.out.println(Property.getProperty("----------------------CONFIGURATION END--------------------"));
+//		System.out.println(Property.getProperty("----------------------CONFIGURATION--------------------"));
+//		System.out.println(Property.getProperty("etutorplusplus.link"));
+//		System.out.println(Property.getProperty("etutorplusplus.extension.key"));
+//		System.out.println(Property.getProperty("semantic.link"));
+//		System.out.println(Property.getProperty("semantic.query.endpoint"));
+//		System.out.println(Property.getProperty("semantic.update.endpoint"));
+//		System.out.println(Property.getProperty("spring.datasource.url"));
+//		System.out.println(Property.getProperty("spring.datasource.username"));
+//		System.out.println(Property.getProperty("spring.datasource.password"));
+//		System.out.println(Property.getProperty("----------------------CONFIGURATION END--------------------"));
 		SpringApplication.run(AprioriApplication.class, args);
 	}
 
